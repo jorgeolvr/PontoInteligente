236a5c6c2cd993f8af27a29d0814a612e9b891fb
Jorge Oliveira <jorgeallancastro@icloud.com>
2021-07-08 10:31:53 -0300

    Entidade Lancamento criado

--- src/main/java/com/group/pontointeligente/api/entities/Lancamento.java
+++ src/main/java/com/group/pontointeligente/api/entities/Lancamento.java
@@ -1,4 +1,121 @@
 package com.group.pontointeligente.api.entities;
 
-public class Lancamento {
+import com.group.pontointeligente.api.enums.TipoEnum;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "lancamento")
+public class Lancamento implements Serializable {
+
+    private static final long serialVersionUID = 6524560251526772839L;
+
+    private Long id;
+    private Date data;
+    private String descricao;
+    private String localizacao;
+    private Date dataCriacao;
+    private Date dataAtualizacao;
+    private TipoEnum tipo;
+    private Funcionario funcionario;
+
+    public Lancamento() {
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "data", nullable = false)
+    public Date getData() {
+        return this.data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    @Column(name = "descricao", nullable = true)
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Column(name = "localizacao", nullable = true)
+    public String getLocalizacao() {
+        return this.localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    @Column(name = "data_criacao", nullable = false)
+    public Date getDataCriacao() {
+        return this.dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    @Column(name = "data_atualizacao", nullable = false)
+    public Date getDataAtualizacao() {
+        return this.dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo", nullable = false)
+    public TipoEnum getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(TipoEnum tipo) {
+        this.tipo = tipo;
+    }
+
+    // Fetch tipo eager serve para trazer o funcionario sempre que buscar um lancameto
+    @ManyToOne(fetch = FetchType.EAGER)
+    public Funcionario getFuncionario() {
+        return this.funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        dataAtualizacao = new Date();
+    }
+
+    @PrePersist
+    public void prePersist() {
+        final Date dataAtual = new Date();
+        this.dataCriacao = dataAtual;
+        this.dataAtualizacao = dataAtual;
+    }
+
+    public String stringfy() {
+        return "Lancamento [id: " + this.id + ", data: " + this.data + ", descricao: " + this.descricao +
+                ", localizacao: " + this.localizacao + ", dataCriacao: " + this.dataCriacao + ", dataAtualizacao: " +
+                this.dataAtualizacao + ", tipo: " + this.tipo + ", funcionario: " + this.funcionario + "]" ;
+    }
 }
